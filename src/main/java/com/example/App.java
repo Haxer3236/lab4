16856325e7b52468b4727106256dca5a862a3e64
Pravesh <pravesh.pal2241@gmail.com>
2025-07-14 11:48:57 +0530

    Commiting modyfied code by Pravesh Pal

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "This is modifyed code for Auto deployment" );
+	System.out.println("This is modifyed code by Pravesh Pal"+"\n"+"Bits ID : 2021wb86502");
+
     }
 }
